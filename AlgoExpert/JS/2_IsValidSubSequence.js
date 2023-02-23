e2950008dab1014d938ac46fa9257e11e38d2d79
George Rykunov <43174725+MercurialG@users.noreply.github.com>
2023-02-22 19:45:46 -0500

    Update 2_IsValidSubSequence.js
    For loop solution

--- AlgoExpert/JS/2_IsValidSubSequence.js
+++ AlgoExpert/JS/2_IsValidSubSequence.js
@@ -1,6 +1,7 @@
 const inputArray = [5, 1, 22, 25, 6, -1, 8, 10];
 const inputSequence = [1, 6, -1, 10];
 
+// O(n) time | O(1) space
 function isValidSubsequenceWhile(array, sequence) {
   let arrPointer = 0;
   let seqPointer = 0;
@@ -14,7 +15,20 @@ function isValidSubsequenceWhile(array, sequence) {
   return seqPointer == sequence.length;
 }
 
+function isValidSubsequenceFor(array, sequence) {
+  let seqPointer = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (seqPointer === sequence.length) return true;
+    if (sequence[seqPointer] === array[i]) {
+      seqPointer++;
+    }
+  }
+  return seqPointer === sequence.length;
+}
+
 console.log(
   "While loop Valid Subsequence",
-  isValidSubsequenceWhile(inputArray, inputSequence)
+  isValidSubsequenceWhile(inputArray, inputSequence),
+  "For loop Valid Subsequence",
+  isValidSubsequenceFor(inputArray, inputSequence)
 );
