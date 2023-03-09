f3d1be16a42af1b8d6922a1f3282746dcbccf61e
George Rykunov <43174725+MercurialG@users.noreply.github.com>
2023-03-08 21:55:34 -0500

    Update 242.ValidAnagram.js

--- LeetCode/242.ValidAnagram.js
+++ LeetCode/242.ValidAnagram.js
@@ -1,3 +1,19 @@
+// O(NLog(N))
+
 var isAnagram = function (s, t) {
   return s.split("").sort().join("") === t.split("").sort().join("");
+};
+// O(S + T) ST
+var isAnagram = function (s, t) {
+  if (s.length != t.length) return false;
+  const hash = {};
+  for (const ch of s) {
+    if (!(ch in hash)) hash[ch] = 0;
+    hash[ch]++;
+  }
+  for (const ch of t) {
+    if (!(ch in hash) || hash[ch] === 0) return false;
+    hash[ch]--;
+  }
+  return true;
 };
