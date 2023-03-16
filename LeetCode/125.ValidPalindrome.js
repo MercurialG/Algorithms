7d0b1680cc2741c23aa4e03f3fb6d4606422d8ed
George Rykunov <43174725+MercurialG@users.noreply.github.com>
2023-03-15 22:24:24 -0500

    Update 125.ValidPalindrome.js

--- LeetCode/125.ValidPalindrome.js
+++ LeetCode/125.ValidPalindrome.js
@@ -8,3 +8,17 @@ var isPalindrome = function (s) {
   }
   return newStr.reverse().join("") === newStr.join("");
 };
+
+var isPalindrome = function (s) {
+  let splitted = s.toLowerCase().split("");
+  let right = splitted.length - 1;
+  for (let left = 0; left < splitted.lenght; left++) {
+    if (/^[a-z0-9]/gi.test(splitted[left])) left++;
+    if (/^[a-z0-9]/gi.test(splitted[right])) right--;
+    if (splitted[left] === splitted[right]) {
+      left++;
+      right--;
+    } else return false;
+  }
+  return true;
+};
